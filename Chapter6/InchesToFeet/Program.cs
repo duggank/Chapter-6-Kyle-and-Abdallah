c82fb396d61c88b274f148487f4ba1a005fc9a10
duggank <CreativeWeep@gmail.com>
2014-09-29 08:40:07 -0400

    Initial Commit V1
    Started the project

---
+++ Chapter6/InchesToFeet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InchesToFeet
+{
+    class Program
+    {
+        public static void InchesToFeet(int inches, int feet, int remain)
+        {
+            feet = inches / 12;
+            remain = inches % 12;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
