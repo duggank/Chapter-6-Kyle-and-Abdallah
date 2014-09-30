1abd2a61219acc01ecc9568f8bf29da9cc622f15
duggank <CreativeWeep@gmail.com>
2014-09-30 08:39:26 -0400

    Initial Commit V2
    We finished Exercise 2.

--- Chapter6/InchesToFeet/Program.cs
+++ Chapter6/InchesToFeet/Program.cs
@@ -7,14 +7,27 @@ namespace InchesToFeet
 {
     class Program
     {
-        public static void InchesToFeet(int inches, int feet, int remain)
-        {
-            feet = inches / 12;
-            remain = inches % 12;
-        }
-
         static void Main(string[] args)
         {
+            int inches = 67;
+            InchesToFeet(inches);
+            InchesToYards(inches);
+        }
+
+        public static void InchesToFeet(int inches)
+        {
+            int feet = inches / 12;
+            int remain = inches % 12;
+            Console.WriteLine("{0} inches = {1} feet and {2} inches.", inches, feet, remain);
+        }
+
+        public static void InchesToYards(int inches)
+        {
+            int feet = inches / 12;
+            int yards = feet / 3;
+            int rfeet = feet - 3;
+            int remain = inches % 12;
+            Console.WriteLine("{0} inches = {1} yard(s), {2} feet, and {3} inches.", inches, yards, rfeet, remain);
         }
     }
 }
